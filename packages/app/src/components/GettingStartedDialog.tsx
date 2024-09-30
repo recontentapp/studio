2ccc615e24badf95c9a736d0304362353bd6ec3b
greeeg <gregoire.mielle@pennylane.com>
2024-09-30 22:33:10 +0200

    feat: add GettingStartedDialog

---
+++ packages/app/src/components/GettingStartedDialog.tsx
@@ -0,0 +1,236 @@
+import {
+  Box,
+  Button,
+  Code,
+  Dialog,
+  Flex,
+  Link,
+  Tabs,
+  Text,
+} from '@radix-ui/themes'
+
+const folderStructure = `.
+├── layouts
+│   └── default
+│       ├── config.json
+│       ├── template.mjml
+│       └── variables.json
+└── welcome
+    ├── config.json
+    ├── template.mjml
+    ├── variables.en.json
+    └── variables.fr.json`
+
+const configFile = `{
+  "name": "Welcome email",
+  "layout": "../layouts/default/template.mjml",
+  "schema": {
+    "type": "object",
+    "properties": {
+      "name": {
+        "type": "string",
+        "example": "John Doe"
+      }
+    },
+    "additionalProperties": false,
+    "required": ["name"]
+  }
+}`
+
+const variablesExample = `<mj-text>{{{ name }}}</mj-text>`
+
+const schemaTypescriptExample = `interface WelcomeEmail {
+  name: string
+  features: {
+    name: string
+  }[]
+}`
+
+const schemaExample = `<mj-section>
+  <mj-column>
+    <mj-text>{{ name }}</mj-text>
+
+    {{ #features }}
+      <mj-text>{{ name }}</mj-text>
+    {{ /features }}
+  </mj-column>
+</mj-section>`
+
+export const GettingStartedDialog = () => {
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger>
+        <Button
+          variant="surface"
+          color="gray"
+          style={{ width: '100%', height: 'inherit' }}
+        >
+          <Flex align="center" gap="2">
+            <svg
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M8.69667 0.0403541C8.90859 0.131038 9.03106 0.354857 8.99316 0.582235L8.0902 6.00001H12.5C12.6893 6.00001 12.8625 6.10701 12.9472 6.27641C13.0319 6.4458 13.0136 6.6485 12.8999 6.80001L6.89997 14.8C6.76167 14.9844 6.51521 15.0503 6.30328 14.9597C6.09135 14.869 5.96888 14.6452 6.00678 14.4178L6.90974 9H2.49999C2.31061 9 2.13748 8.893 2.05278 8.72361C1.96809 8.55422 1.98636 8.35151 2.09999 8.2L8.09997 0.200038C8.23828 0.0156255 8.48474 -0.0503301 8.69667 0.0403541ZM3.49999 8.00001H7.49997C7.64695 8.00001 7.78648 8.06467 7.88148 8.17682C7.97648 8.28896 8.01733 8.43723 7.99317 8.5822L7.33027 12.5596L11.5 7.00001H7.49997C7.353 7.00001 7.21347 6.93534 7.11846 6.8232C7.02346 6.71105 6.98261 6.56279 7.00678 6.41781L7.66968 2.44042L3.49999 8.00001Z"
+                fill="currentColor"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+
+            <Flex direction="column" align="start" py="2">
+              <Text align="left" size="1">
+                How does it work?
+              </Text>
+              <Text align="left" size="1" style={{ color: 'var(--gray-9)' }}>
+                Managing templates step-by-step
+              </Text>
+            </Flex>
+          </Flex>
+        </Button>
+      </Dialog.Trigger>
+
+      <Dialog.Content
+        minHeight="520px"
+        style={{ display: 'flex', flexDirection: 'column' }}
+      >
+        <Tabs.Root defaultValue="folder_structure" style={{ flexGrow: 1 }}>
+          <Tabs.List>
+            <Tabs.Trigger value="folder_structure">
+              Folder structure
+            </Tabs.Trigger>
+            <Tabs.Trigger value="config">Config file</Tabs.Trigger>
+            <Tabs.Trigger value="variables">Variables</Tabs.Trigger>
+            <Tabs.Trigger value="schema">JSON schema</Tabs.Trigger>
+            <Tabs.Trigger value="compile">Compile</Tabs.Trigger>
+          </Tabs.List>
+
+          <Box pt="3">
+            <Tabs.Content value="folder_structure">
+              <Flex gap="3" direction="column">
+                <Text size="2">
+                  Templates & layouts are organized in folders. Variables can be
+                  localized.
+                </Text>
+
+                <pre
+                  style={{
+                    margin: 0,
+                    fontSize: 'var(--font-size-1)',
+                    backgroundColor: 'var(--accent-a3)',
+                    color: 'var(--accent-a11)',
+                    padding: 'var(--space-4) var(--space-3)',
+                    borderRadius: 'var(--radius-2)',
+                  }}
+                >
+                  {folderStructure}
+                </pre>
+              </Flex>
+            </Tabs.Content>
+
+            <Tabs.Content value="config">
+              <Flex gap="3" direction="column">
+                <Text size="2">
+                  Config files contain the template's metadata like the layout
+                  to use or a JSON schema for data that can be passed to the
+                  template.
+                </Text>
+
+                <pre
+                  style={{
+                    margin: 0,
+                    fontSize: 'var(--font-size-1)',
+                    backgroundColor: 'var(--accent-a3)',
+                    color: 'var(--accent-a11)',
+                    padding: 'var(--space-4) var(--space-3)',
+                    borderRadius: 'var(--radius-2)',
+                  }}
+                >
+                  {configFile}
+                </pre>
+              </Flex>
+            </Tabs.Content>
+
+            <Tabs.Content value="variables">
+              <Flex gap="3" direction="column">
+                <Text size="2">
+                  Variables are stored in JSON files & can be used using the{' '}
+                  <Code>{'{{{ name }}}'}</Code> syntax.
+                </Text>
+
+                <pre
+                  style={{
+                    margin: 0,
+                    fontSize: 'var(--font-size-1)',
+                    backgroundColor: 'var(--accent-a3)',
+                    color: 'var(--accent-a11)',
+                    padding: 'var(--space-4) var(--space-3)',
+                    borderRadius: 'var(--radius-2)',
+                  }}
+                >
+                  {variablesExample}
+                </pre>
+              </Flex>
+            </Tabs.Content>
+
+            <Tabs.Content value="schema">
+              <Flex gap="3" direction="column">
+                <Text size="2">
+                  The schema allows passing data to the template. It uses{' '}
+                  <Link
+                    href="https://mustache.github.io/mustache.5.html"
+                    target="_blank"
+                  >
+                    Mustache
+                  </Link>{' '}
+                  with the following <Code>{'{{ name }}'}</Code> syntax.
+                </Text>
+
+                <pre
+                  style={{
+                    margin: 0,
+                    fontSize: 'var(--font-size-1)',
+                    backgroundColor: 'var(--accent-a3)',
+                    color: 'var(--accent-a11)',
+                    padding: 'var(--space-4) var(--space-3)',
+                    borderRadius: 'var(--radius-2)',
+                  }}
+                >
+                  {schemaTypescriptExample}
+                </pre>
+
+                <pre
+                  style={{
+                    margin: 0,
+                    fontSize: 'var(--font-size-1)',
+                    backgroundColor: 'var(--accent-a3)',
+                    color: 'var(--accent-a11)',
+                    padding: 'var(--space-4) var(--space-3)',
+                    borderRadius: 'var(--radius-2)',
+                  }}
+                >
+                  {schemaExample}
+                </pre>
+              </Flex>
+            </Tabs.Content>
+
+            <Tabs.Content value="compile">
+              <Text size="2">TO DO</Text>
+            </Tabs.Content>
+          </Box>
+        </Tabs.Root>
+
+        <Flex gap="3" mt="4" justify="end">
+          <Dialog.Close>
+            <Button variant="soft" color="gray">
+              Close
+            </Button>
+          </Dialog.Close>
+        </Flex>
+      </Dialog.Content>
+    </Dialog.Root>
+  )
+}
--- packages/app/src/components/Sidebar.tsx
+++ packages/app/src/components/Sidebar.tsx
@@ -1,58 +1,63 @@
-import { Box, Flex, Text } from '@radix-ui/themes'
+import { Flex, Text } from '@radix-ui/themes'
 import { PropsWithChildren } from 'react'
+import { GettingStartedDialog } from './GettingStartedDialog'
 
 export const Sidebar = ({ children }: PropsWithChildren) => {
   return (
-    <Box
+    <Flex
+      direction="column"
+      position="relative"
       width="270px"
       height="100vh"
       flexShrink="0"
-      overflowX="hidden"
-      overflowY="auto"
       style={{ borderRight: '1px solid var(--gray-3)' }}
     >
-      <Flex px="3" pt="0" pb="5" direction="column">
-        <Flex
-          position="sticky"
-          style={{ zIndex: 5, backgroundColor: 'var(--color-background)' }}
-          top="0"
-          direction="row"
-          align="center"
-          gap="2"
-          pt="4"
-          pb="5"
-          px="2"
+      <Flex
+        direction="row"
+        align="center"
+        gap="2"
+        p="4"
+      >
+        <svg
+          width="20"
+          height="20"
+          viewBox="0 0 200 200"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          <svg
-            width="20"
-            height="20"
-            viewBox="0 0 200 200"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M0 100C0 24 24 0 100 0s100 24 100 100-24 100-100 100S0 176 0 100Z"
-              fill="var(--gray-contrast)"
-            ></path>
-            <path
-              fill="var(--color-background)"
-              d="M42.4 94.8V42h52.8v52.8z"
-            ></path>
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M104.8 42v120c33.137 0 60-26.863 60-60s-26.863-60-60-60ZM63.399 161.509l33.807-22.639C94.554 152.06 82.826 162 68.76 162c-1.832 0-3.624-.168-5.361-.491ZM47.085 152.34l47.675-31.926a28.452 28.452 0 0 1 2.841 9.677L55.05 158.586a29.079 29.079 0 0 1-7.964-6.246ZM90.842 114.523l-47.8 32.011A28.454 28.454 0 0 1 40 136.99l43.073-28.845a29.106 29.106 0 0 1 7.768 6.378ZM40.158 128.37c2.314-13.606 14.24-23.97 28.602-23.97 2.13 0 4.206.228 6.206.661L40.158 128.37Z"
-              fill="var(--color-background)"
-            ></path>
-          </svg>
+          <path
+            d="M0 100C0 24 24 0 100 0s100 24 100 100-24 100-100 100S0 176 0 100Z"
+            fill="var(--gray-contrast)"
+          ></path>
+          <path
+            fill="var(--color-background)"
+            d="M42.4 94.8V42h52.8v52.8z"
+          ></path>
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M104.8 42v120c33.137 0 60-26.863 60-60s-26.863-60-60-60ZM63.399 161.509l33.807-22.639C94.554 152.06 82.826 162 68.76 162c-1.832 0-3.624-.168-5.361-.491ZM47.085 152.34l47.675-31.926a28.452 28.452 0 0 1 2.841 9.677L55.05 158.586a29.079 29.079 0 0 1-7.964-6.246ZM90.842 114.523l-47.8 32.011A28.454 28.454 0 0 1 40 136.99l43.073-28.845a29.106 29.106 0 0 1 7.768 6.378ZM40.158 128.37c2.314-13.606 14.24-23.97 28.602-23.97 2.13 0 4.206.228 6.206.661L40.158 128.37Z"
+            fill="var(--color-background)"
+          ></path>
+        </svg>
 
-          <Text size="2" weight="medium">
-            Recontent Studio
-          </Text>
-        </Flex>
+        <Text size="2" weight="medium">
+          Recontent Studio
+        </Text>
+      </Flex>
 
+      <Flex direction="column" width="100%" flexGrow="1" overflowY="auto" px="3" pb="4" gap="3">
         {children}
       </Flex>
-    </Box>
+
+      <Flex
+        p="3"
+        style={{
+          borderTop: '1px solid var(--gray-3)',
+        }}
+      >
+        <GettingStartedDialog />
+      </Flex>
+    </Flex>
   )
 }
